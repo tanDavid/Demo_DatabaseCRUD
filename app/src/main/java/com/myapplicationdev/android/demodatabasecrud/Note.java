e9fa17840202f935352cac906ea5305fd12a795e
19002765 <19002765@myrp.edu.sg>
2021-05-18 13:14:17 +0800

    completed

---
+++ app/src/main/java/com/myapplicationdev/android/demodatabasecrud/Note.java
@@ -0,0 +1,30 @@
+package com.myapplicationdev.android.demodatabasecrud;
+
+import java.io.Serializable;
+
+public class Note implements Serializable {
+    private int id;
+    private String noteContent;
+
+    public Note(int id, String noteContent) {
+        this.id = id;
+        this.noteContent = noteContent;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    @Override
+    public String toString() {
+        return "ID:" + id + ", " + noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+}
